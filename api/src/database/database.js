86f6d88da80567cdb32bff9ce7bf25d514f161c1
TomCostabel <tomasperalta1997@hotmail.com>
2023-03-13 16:47:41 -0300

    deploy backend

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
-const { DB_PASSWORD, DB_USER } = process.env;
+// const { DB_PASSWORD, DB_USER } = process.env;
+const { MONGOUSER, MONGOPASSWORD, MONGOHOST, MONGOPORT } = process.env;
 
-const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ssblcmh.mongodb.net/DBproducts`;
+// const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ssblcmh.mongodb.net/DBproducts`;
+const url = `mongodb+srv://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}/${MONGOPORT}`;
 
+("mongodb://mongo:FiblLN20BxZqsIZBfxSB@containers-us-west-146.railway.app:5916");
 // Con strictQuery estoy denegando un cambio que se viene en mongoDB 7
 mongoose.set("strictQuery", false);
 mongoose
