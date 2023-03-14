ad96d03194c6cabc8e4716d3989b7e9f1736f4a4
TomCostabel <tomasperalta1997@hotmail.com>
2023-03-14 11:10:26 -0300

    deploy back

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,14 @@
 import app from "./app.js";
 import "./database/database.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const { MONGOPORT } = process.env;
+
 async function main() {
     try {
-        app.listen(3001);
-        console.log("server in listening on port 💡", 3001);
+        app.listen(MONGOPORT);
+        console.log("server in listening on port 💡", MONGOPORT);
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
