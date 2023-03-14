65523c222bea47b40cc85866bdbe1978b4d14ca8
TomCostabel <tomasperalta1997@hotmail.com>
2023-03-14 11:43:28 -0300

    deploy bugs

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -5,13 +5,15 @@ dotenv.config();
 const { MONGOUSER, MONGOPASSWORD, MONGOHOST, MONGOPORT } = process.env;
 
 // const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ssblcmh.mongodb.net/DBproducts`;
-const url = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`;
+// const url = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`;
 
 // ("mongodb://mongo:FiblLN20BxZqsIZBfxSB@containers-us-west-146.railway.app:5916");
 // Con strictQuery estoy denegando un cambio que se viene en mongoDB 7
 mongoose.set("strictQuery", false);
 mongoose
-    .connect(url, {})
+    // .connect(url, {})
+    .connect(process.env.MONGO_URL, {})
+
     .then(() => {
         console.log("Base de datos conectada 🔋🔌");
     })
--- api/src/index.js
+++ api/src/index.js
@@ -3,12 +3,12 @@ import "./database/database.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.MONGOPORT || 3001;
+const Port = process.env.PORT || 3001;
 
 async function main() {
     try {
-        app.listen(PORT);
-        console.log("server in listening on port 💡", PORT);
+        app.listen(Port);
+        console.log("server in listening on port 💡", Port);
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
